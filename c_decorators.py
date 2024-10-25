ab25a7e97992546e59ab1ea9c1273a5199ffeff6
ioanvl <ioannis.vlasso@gmail.com>
2024-10-25 16:34:49 +0200

    deco for iterable

--- c_decorators.py
+++ c_decorators.py
@@ -4,6 +4,7 @@ from json.decoder import JSONDecodeError
 from pathlib import Path
 from typing import Callable, TypeVar
 from cli_utils import clear_line
+from collections.abc import Iterable
 
 T = TypeVar("T")
 
@@ -24,24 +25,28 @@ def safe_parse_per_file(
 
             print(f"-- Parsing {component_attr.replace('_', ' ').title()}")
 
-            location = getattr(self.component_location, component_attr)
+            locations = getattr(self.component_location, component_attr)
+            if isinstance(locations, Path):
+                locations = [locations]
 
-            for file_path in location.rglob(file_pattern):
-                try:
-                    try:
-                        with file_path.open() as f:
-                            data = json.load(f)
-                    except (UnicodeDecodeError, JSONDecodeError) as _:
-                        if DEBUG:
-                            print(f"WARN!! - {file_path}")
-                            _ = input()
-                        continue
+            if isinstance(locations, Iterable):
+                for location in locations:
+                    for file_path in location.rglob(file_pattern):
+                        try:
+                            try:
+                                with file_path.open() as f:
+                                    data = json.load(f)
+                            except (UnicodeDecodeError, JSONDecodeError) as _:
+                                if DEBUG:
+                                    print(f"WARN!! - {file_path}")
+                                    _ = input()
+                                continue
 
-                    func(self, file_path, data, *args, **kwargs)
+                            func(self, file_path, data, *args, **kwargs)
 
-                except Exception as e:
-                    print(f"\n\n{file_path}\n\n")
-                    raise e
+                        except Exception as e:
+                            print(f"\n\n{file_path}\n\n")
+                            raise e
 
             if not self.verbose:
                 print(clear_line, end="")
