eb04023127f85407b12497727e6cc3304abeacb6
ioanvl <ioannis.vlasso@gmail.com>
2024-11-12 21:14:34 +0100

    more misc

--- src/__main__.py
+++ src/__main__.py
@@ -3,7 +3,11 @@ from constants.runtime_const import CrOpType, gcr_settings
 
 if __name__ == "__main__":
     gcr_settings.OP_MODE = CrOpType(1)
-    comb = Combiner()
-    comb.run()
+    try:
+        comb = Combiner()
+        comb.run()
+    except Exception as e:
+        print("An ERROR occured:")
+        print(e)
 
     _ = input("\n\nPress [Enter] to exit..")
